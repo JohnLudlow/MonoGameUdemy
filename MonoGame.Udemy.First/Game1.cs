df6668c2e596152cf63920e9109bec13983a45c3
John Ludlow <john.ludlow@halfords.co.uk>
2025-04-01 12:28:54 +0100

    #25 MonoGame Tutorial - screen edge detection and sound effects

--- MonoGame.Udemy.First/Game1.cs
+++ MonoGame.Udemy.First/Game1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using System.Globalization;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using MonoGameReload;
 
 namespace MonoGame.Udemy.First;
@@ -12,14 +14,20 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    Texture2D _daffySprite;
-    Vector2 _daffySpritePosition;
+    private Texture2D _daffySprite;
+    private Vector2 _daffySpritePosition;
 
-    Texture2D _minnieSprite;
-    Vector2 _minnieSpritePosition;
+    private Texture2D _minnieSprite;
+    private Vector2 _minnieSpritePosition;
 
+    private SpriteFont _scoreFont;
 
-    Vector2 _spriteVelocity = new(5, 1);
+    private int _score;
+
+    private Vector2 _spriteVelocity = new(5, 1);
+
+    private Song _introSong;
+    private SoundEffect _arrowSoundEffect;
 
     public Game1()
     {
@@ -51,15 +59,37 @@ public class Game1 : Game
         // TODO: use this.Content to load your game content here
         _daffySprite = Content.Load<Texture2D>("Assets/Images/daffy");        
         _minnieSprite = Content.Load<Texture2D>("Assets/Images/cartoon");        
+        _scoreFont = Content.Load<SpriteFont>("Assets/Fonts/ScoreFont");
+        _introSong = Content.Load<Song>("Assets/Sounds/intro");
+        _arrowSoundEffect = Content.Load<SoundEffect>("Assets/Sounds/arrow");
+
+        MediaPlayer.Play(_introSong);
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+        // TODO: Add your update logic here
 
         _daffySpritePosition += _spriteVelocity;
-        // TODO: Add your update logic here
+
+        if (_daffySpritePosition.X + _daffySprite.Width  > _graphics.GraphicsDevice.Viewport.Width)
+        {
+            _spriteVelocity.X = -_spriteVelocity.X;
+            _score++;
+            _arrowSoundEffect.Play();
+        }
+
+        if (_daffySpritePosition.X                       < 0)
+            _spriteVelocity.X = -_spriteVelocity.X;
+
+        if (_daffySpritePosition.Y + _daffySprite.Height > _graphics.GraphicsDevice.Viewport.Height)
+            _spriteVelocity.Y = -_spriteVelocity.Y;
+
+        if (_daffySpritePosition.Y                       < 0)
+            _spriteVelocity.Y = -_spriteVelocity.Y;
+
 
         base.Update(gameTime);
     }
@@ -75,6 +105,7 @@ public class Game1 : Game
             _spriteBatch.Begin();
             _spriteBatch.Draw(_daffySprite, _daffySpritePosition, Color.White);
             _spriteBatch.Draw(_minnieSprite, _minnieSpritePosition, Color.White);
+            _spriteBatch.DrawString(_scoreFont, _score.ToString(CultureInfo.DefaultThreadCurrentUICulture), new Vector2(10, 10), Color.White);
         }
         finally
         {
